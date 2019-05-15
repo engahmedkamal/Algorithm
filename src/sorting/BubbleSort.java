c617ffbfeab01c86cafed1d7cf280ab9c883b70e
ahmedkamal <eng.ahmedkamal91@gmail.com>
2019-05-15 23:08:39 +0200

    bubble sort Algorithm

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,4 +1,20 @@
 package sorting;
 
 public class BubbleSort {
+    public void sort(int arry[]) {
+        for (int i = 0; i < arry.length - 1; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < arry.length - i - 1; j++) {
+                if (arry[j] > arry[j + 1]) {
+                    int temp = arry[j];
+                    arry[j] = arry[j + 1];
+                    arry[j + 1] = temp;
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+    }
 }
